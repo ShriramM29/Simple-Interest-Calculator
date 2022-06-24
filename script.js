664b36b8117d3da65c0e01dc0a8bc71d16fb65e1
ShriramM29 <96107004+ShriramM29@users.noreply.github.com>
2022-06-24 21:52:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-function compute(){
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = money_round(principal * years * rate / 100);
-    var year = new Date().getFullYear()+parseInt(years);
-
-    if(isNaN(principal) || principal <= 0){
-        alert("Enter a positive number.")
-        principal.focus();
-        return;
+function compute() {
+    if(document.getElementById("principal").value>0) {
+        var princ = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var result = princ*rate*years/100;
+        for (var i=0; i<2021; i++)years++;
+        document.getElementById("result").innerHTML=result;
+        document.getElementById("princout").innerHTML=princ;
+        document.getElementById("rateout").innerHTML=rate;
+        document.getElementById("yearout").innerHTML=(years);
+        document.getElementById("Answer").style.display="block";
+    } 
+    else {
+        alert("Please enter positive value as principal");
+        document.getElementById("principal").value = '\0';
+        document.getElementById("Answer").style.display="none";
+        document.getElementById("principal").focus();
     }
-
-    //to round up to two decimal places
-    function money_round(num) {
-        return Math.ceil(num * 100) / 100;
-    }
-
-    var result = document.getElementById("result");
-    return result.innerText = "If you deposit $" + principal + ",\n at an interest rate of " + rate +"%.\n  You will receive an amount of $" + interest + ",\n in the year of " + year;
 }
-        
-
-//function to read the values of the range slider and display it in the <span> adjacent to the slider
-function updateRate(){
-    var rateVal = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateVal + "%";
+function update() {
+    var values=document.getElementById("rate").value;
+    //values = values.toFixed(1);
+    document.getElementById("rangeout").innerHTML = values;
 }
-        
